e9a1ea6f90f8c84bdd093a7cc33d1d3dcb9b5070
Md Anisur Rahman <anistuhin@gmail.com>
2022-04-26 08:41:16 -0500

    Added require JS

--- scripts.js
+++ scripts.js
@@ -94,77 +94,207 @@
         $('.emissions-sort').val(0);
 
         // Bar Chart
-        var margin = { top: 40, right: 20, bottom: 30, left: 40 },
-            width = 680 - margin.left - margin.right,
-            height = 350 - margin.top - margin.bottom;
-
-        var formatPercent = d3.format(".0%");
-
-        var x = d3.scale.ordinal()
-            .rangeRoundBands([0, width], .1);
-
-        var y = d3.scale.linear()
-            .range([height, 0]);
-
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom");
-
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient("left");
-
-        var tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([-10, 0])
-            .html(function(d) {
-                return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
-            })
-
-        var svg = d3.select("#barchart").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        svg.call(tip);
-
-        d3.csv("data.csv", type, function(error, data) {
-            x.domain(data.map(function(d) { return d.letter; }));
-            y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
-
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
-
-            svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text("Frequency");
-
-            svg.selectAll(".bar")
-                .data(data)
-                .enter().append("rect")
-                .attr("class", "bar")
-                .attr("x", function(d) { return x(d.letter); })
-                .attr("width", x.rangeBand())
-                .attr("y", function(d) { return y(d.frequency); })
-                .attr("height", function(d) { return height - y(d.frequency); })
-                .on('mouseover', tip.show)
-                .on('mouseout', tip.hide)
-
+        require(["d3.v3.min.js"], function(d3) {
+            var margin = { top: 40, right: 20, bottom: 30, left: 40 },
+                width = 680 - margin.left - margin.right,
+                height = 350 - margin.top - margin.bottom;
+            var formatPercent = d3.format(".0%");
+            var x = d3.scale.ordinal()
+                .rangeRoundBands([0, width], .1);
+            var y = d3.scale.linear()
+                .range([height, 0]);
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom");
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient("left");
+            var tip = d3.tip()
+                .attr('class', 'd3-tip')
+                .offset([-10, 0])
+                .html(function(d) {
+                    return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
+                })
+            var svg = d3.select("#barchart").append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            svg.call(tip);
+            d3.csv("data.csv", type, function(error, data) {
+                x.domain(data.map(function(d) { return d.letter; }));
+                y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+                svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
+                svg.append("g")
+                    .attr("class", "y axis")
+                    .call(yAxis)
+                    .append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", ".71em")
+                    .style("text-anchor", "end")
+                    .text("Frequency");
+                svg.selectAll(".bar")
+                    .data(data)
+                    .enter().append("rect")
+                    .attr("class", "bar")
+                    .attr("x", function(d) { return x(d.letter); })
+                    .attr("width", x.rangeBand())
+                    .attr("y", function(d) { return y(d.frequency); })
+                    .attr("height", function(d) { return height - y(d.frequency); })
+                    .on('mouseover', tip.show)
+                    .on('mouseout', tip.hide)
+            });
+            function type(d) {
+                d.frequency = +d.frequency;
+                return d;
+            }
         });
+        // pie chart
+        require(["d3.v4.min.js"], function(d3) {
+            var myDuration = 600;
+            var firstTime = true;
+            var width = 250,
+                height = 250,
+                radius = Math.min(width, height) / 2;
+            var width = 250,
+                height = 250,
+                radius = Math.min(width, height) / 2;
+            var color = d3.scaleOrdinal(d3.schemeCategory20);
+            var pie = d3.pie()
+                .value(function(d) { return d.count; })
+                .sort(null);
+            var arc = d3.arc()
+                .innerRadius(radius - 100)
+                .outerRadius(radius - 20);
+            var svg = d3.select("#pie-chart").append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .append("g")
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+            d3.tsv("data.tsv", type, function(error, data) {
+                var regionsByFruit = d3.nest()
+                    .key(function(d) { return d.fruit; })
+                    .entries(data)
+                    .reverse();
+                var label = d3.select("form").selectAll("label")
+                    .data(regionsByFruit)
+                    .enter().append("label");
+                label.append("input")
+                    .attr("type", "radio")
+                    .attr("name", "fruit")
+                    .attr("value", function(d) { return d.key; })
+                    .on("change", change)
+                    .filter(function(d, i) { return !i; })
+                    .each(change)
+                    .property("checked", true);
+                label.append("span")
+                    .text(function(d) { return d.key; });
+                function change(region) {
+                    var path = svg.selectAll("path");
+                    var data0 = path.data(),
+                        data1 = pie(region.values);
+                    path = path.data(data1, key);
+                    path
+                        .transition()
+                        .duration(myDuration)
+                        .attrTween("d", arcTween)
+                    path
+                        .enter()
+                        .append("path")
+                        .each(function(d, i) {
+                            var narc = findNeighborArc(i, data0, data1, key);
+                            if (narc) {
+                                this._current = narc;
+                                this._previous = narc;
+                            } else {
+                                this._current = d;
+                            }
+                        })
+                        .attr("fill", function(d, i) {
+                            return color(d.data.region)
+                        })
+                        .transition()
+                        .duration(myDuration)
+                        .attrTween("d", arcTween)
+                    path
+                        .exit()
+                        .transition()
+                        .duration(myDuration)
+                        .attrTween("d", function(d, index) {
+                            var currentIndex = this._previous.data.region;
+                            var i = d3.interpolateObject(d, this._previous);
+                            return function(t) {
+                                return arc(i(t))
+                            }
+                        })
+                        .remove()
+                    firstTime = false;
+                }
+            });
 
-        function type(d) {
-            d.frequency = +d.frequency;
-            return d;
-        }
-        window.d3 === undefined;
+            function key(d) {
+                return d.data.region;
+            }
+
+            function type(d) {
+                d.count = +d.count;
+                return d;
+            }
+
+            function findNeighborArc(i, data0, data1, key) {
+                var d;
+                if (d = findPreceding(i, data0, data1, key)) {
+                    var obj = cloneObj(d)
+                    obj.startAngle = d.endAngle;
+                    return obj;
+                } else if (d = findFollowing(i, data0, data1, key)) {
+                    var obj = cloneObj(d)
+                    obj.endAngle = d.startAngle;
+                    return obj;
+                }
+                return null
+
+
+            }
+            // Find the element in data0 that joins the highest preceding element in data1.
+            function findPreceding(i, data0, data1, key) {
+                var m = data0.length;
+                while (--i >= 0) {
+                    var k = key(data1[i]);
+                    for (var j = 0; j < m; ++j) {
+                        if (key(data0[j]) === k) return data0[j];
+                    }
+                }
+            }
+            // Find the element in data0 that joins the lowest following element in data1.
+            function findFollowing(i, data0, data1, key) {
+                var n = data1.length,
+                    m = data0.length;
+                while (++i < n) {
+                    var k = key(data1[i]);
+                    for (var j = 0; j < m; ++j) {
+                        if (key(data0[j]) === k) return data0[j];
+                    }
+                }
+            }
+            function arcTween(d) {
+                var i = d3.interpolate(this._current, d);
+                this._current = i(0);
+                return function(t) {
+                    return arc(i(t))
+                }
+            }
+            function cloneObj(obj) {
+                var o = {};
+                for (var i in obj) {
+                    o[i] = obj[i];
+                }
+                return o;
+            }
+        });
     });
 })(window.jQuery);
