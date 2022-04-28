247d8a44108c93c6067a9a64507335aeddbc5951
Md Anisur Rahman <anistuhin@gmail.com>
2022-04-28 06:32:34 -0500

    Done with all validations and undo functions

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,59 @@
 (function($) {
     $(document).ready(function() {
-        var undo = false;
+        var validation = 0;
+        $(document).on('input', '.garbage-row input', function() {
+            $(this).removeClass('has-error');
+        });
+        $(document).on('change', '.garbage-row select', function() {
+            $(this).removeClass('has-error');
+        });
+        $(document).on('click', '#home-next, #add-garbage-btn', function() {
+            $.each($('.garbage-row input'), function() {
+                if ($(this).val().trim() == '') {
+                    $(this).addClass('has-error');
+                    validation += 1;
+                }
+            });
+            $.each($('.garbage-row select'), function() {
+                if ($(this).val() == 0) {
+                    $(this).addClass('has-error');
+                    validation += 1;
+                }
+            });
+            if (validation == 0) {
+                $('.error-msg').remove();
+                if ($(this).attr('id').trim() == 'home-next') {
+                    $('#main').attr("data-id", '1');
+                }
+                if ($(this).attr('id').trim() == 'add-garbage-btn') {
+                    $('.garbage-row .cb-input').prop('readonly', true);
+                    $('.garbage-row select').prop('disabled', true);
+                    $(this).before('<button class="cb-button cb-button-secondary edit-garbage-btn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32zM421.7 220.3L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3z"/></svg></button>');
+                    $(this).before('<button class="cb-button cb-button-secondary delete-garbage-btn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"/></svg></button>');
+                    $(this).before('<button class="cb-button" id="undo-garbage-btn">Undo</button>')
+                    $(this).parent().after(`
+                        <div class="garbage-row" data-id="0">
+                            <input class="cb-input" type="text" name="type" placeholder="Garbage type*">
+                            <input class="cb-input" type="text" name="amount" placeholder="Qty*">
+                            <select class="cb-input" name="qty">
+                                <option value="0">Unit*</option>
+                                <option value="1">Count</option>
+                                <option value="2">Oz</option>
+                                <option value="3">lb</option>
+                                <option value="4">Kg</option>
+                            </select>
+                            <button class="cb-button" id="add-garbage-btn">Add</button>
+                        </div>
+                    `);
+                    $(this).remove();
+                }
+            } else {
+                validation = 0;
+                if($('.error-msg').length == 0) {
+                    $('.add-garbage-section .section-controls').append('<p class="error-msg">All the add garbage fields are required.</p>');
+                }
+            }
+        });
         $(document).on('click', '#logo', function() {
             $('#home').click();
         });
@@ -11,39 +64,20 @@
             $('#modal').addClass('active');
         });
         $(document).on('click', '#add-garbage-btn', function() {
-            $('.garbage-row .cb-input').prop('readonly', true);
-            $('.garbage-row select').prop('disabled', true);
-            $(this).before('<button class="cb-button cb-button-secondary edit-garbage-btn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32zM421.7 220.3L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3z"/></svg></button>');
-            $(this).before('<button class="cb-button cb-button-secondary delete-garbage-btn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"/></svg></button>');
-            $(this).before('<button class="cb-button" id="undo-garbage-btn">Undo</button>')
-            $(this).parent().after(`
-                <div class="garbage-row">
-                    <input class="cb-input" type="text" name="type" placeholder="Garbage type">
-                    <input class="cb-input" type="text" name="amount" placeholder="Quantity">
-                    <select class="cb-input" name="qty">
-                        <option>Unit</option>
-                        <option>Count</option>
-                        <option>Oz</option>
-                        <option>lb</option>
-                        <option>Kg</option>
-                    </select>
-                    <button class="cb-button" id="add-garbage-btn">Add</button>
-                </div>
-            `);
-            $(this).remove();
+
         });
         $(document).on('click', '.delete-garbage-btn', function() {
             var $this = $(this);
             $this.parent('.garbage-row').addClass('active');
-            undo = true;
+            $this.parent('.garbage-row').attr('data-id', 1);
             setTimeout(function() {
-                if (undo) {
+                if ($this.parent('.garbage-row').attr('data-id') == 1) {
                     $this.parent('.garbage-row').remove();
                 }
             }, 5000);
         });
         $(document).on('click', '#undo-garbage-btn', function() {
-            undo = false;
+            $(this).parent('.garbage-row').attr('data-id', 0);
             $(this).parent('.garbage-row').removeClass('active');
         });
         $(document).on('click', '.edit-garbage-btn', function() {
@@ -59,10 +93,6 @@
         });
         $(document).on('click', '#home', function() {
             $('#main').attr("data-id", '0');
-        });
-        $(document).on('click', '#home-next', function() {
-            $('#main').attr("data-id", '1');
-            // $('#home').removeClass('active');
         });
         $(document).on('click', '#choices-prev', function() {
             $('#main').attr("data-id", '0');
